5765a63168a90a9ce1f7e83b2958ee902dc76220
Sudono Tanjung <sudono007@gmail.com>
2018-01-27 03:17:46 +0000

    Adding execution time calculation

--- src/Executor.java
+++ src/Executor.java
@@ -5,11 +5,16 @@ public class Executor {
 	 static LinkedList<Integer> value = new LinkedList<Integer>();
 
 	    public static void main(String[] args){
-	        for (int i = 0; i < 10; i++) {
-				value.add(new Random().nextInt(50 - 1 + 1) + 1);
+	        for (int i = 0; i < 1000; i++) {
+				value.add(new Random().nextInt(200 - 1 + 1) + 1);
 			}
 			System.out.println(value);
+			long startTime = System.currentTimeMillis();
 			SortingCollection.pancakeSort(value);
+			long endTime = System.currentTimeMillis();
 			System.out.println(value);
+			System.out.print("Execution Time : ");
+			System.out.println((endTime - startTime) + " milliseconds");
+			
 	    }
 }
