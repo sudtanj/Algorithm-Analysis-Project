a3c4d7d04fadc2b091cbe07f251ae6929725e05b
Sudono Tanjung <sudono007@gmail.com>
2018-02-02 04:40:49 +0000

    Adding milliseconds calculation

--- src/Executor.java
+++ src/Executor.java
@@ -5,22 +5,23 @@ public class Executor {
 	 static LinkedList<Integer> value = new LinkedList<Integer>();
 
 	    public static void main(String[] args){
-	        for (int i = 0; i < 10; i++) {
+	        for (int i = 0; i < 100; i++) {
 				value.add(new Random().nextInt(200 - 1 + 1) + 1);
 			}
 			System.out.println("Data before sorted :");
 			System.out.println(value);
 			long startTime = System.nanoTime();    
-			//SortingCollection.pancakeSort(value);
+			SortingCollection.pancakeSort(value);
 			//SortingCollection.shellSort(value);
 			//SortingCollection.oyelamiSort(value, value.size());
-			SortingCollection.aviSort(value);
+			//SortingCollection.aviSort(value);
 			long endTime = System.nanoTime();
+			long estimatedTime = (endTime - startTime);
 			System.out.println("Data after sorted :");
 			System.out.println(value);
 			System.out.println("Execution Time : ");
-			System.out.println("- " + (endTime - startTime) + " nanoseconds");
-			
+			System.out.println("- " + estimatedTime + " nanoseconds");
+			System.out.println("- " + (estimatedTime / 1000000.0) + " milliseconds");
 			
 	    }
 }
