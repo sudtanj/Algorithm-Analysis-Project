0422ddf1d908d95d83be2dc99db698b04705d46d
Sudono Tanjung <sudono007@gmail.com>
2018-02-10 12:53:08 +0000

    Add all report

--- src/Executor.java
+++ src/Executor.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class Executor {
 	 static ArrayList<Integer> value = new ArrayList<Integer>();
 	 static PrintStream outone = null;
-	 static String sortName = "Avi_Sort";
+	 static String sortName = "Oyelami_Sort";
 
 	    public static void main(String[] args){
 	    	listFilesAndFolders("test_case");
@@ -28,8 +28,8 @@ public class Executor {
 			long startTime = System.nanoTime();
 			//SortingCollection.pancakeSort(value);
 			//SortingCollection.shellSort(value);
-			//SortingCollection.oyelamiSort(value, value.size());
-			SortingCollection.aviSort(value);
+			SortingCollection.oyelamiSort(value, value.size());
+			//SortingCollection.aviSort(value);
 			long endTime = System.nanoTime();
 			long estimatedTime = (endTime - startTime);
 			System.out.println("Data after sorted :");
